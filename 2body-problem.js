04f5d0a607563d01c630976bb953eb21cfafdcef
Michael Rubanov <persorub@personalics.com>
2020-01-10 02:22:46 +0200

    a mess , @wip

---
+++ 2body-problem.js
@@ -0,0 +1,13 @@
+Bulk parameters
+	Sun	Earth	"Ratio
+(Sun/Earth)"
+Mass (1024 kg)	1,988,500.00	5.9724	333,000.00
+GM (x 106 km3/s2)	132,712.00	0.3986	333,000.00
+Volume (1012 km3)	1,412,000.00	1.083	1,304,000.00
+Volumetric mean radius (km)	695,700.00	6371	109.2
+Mean density (kg/m3)	1408	5514	0.255
+Surface gravity (eq.) (m/s2)	274	9.78	28
+Escape velocity (km/s)	617.6	11.19	55.2
+Ellipticity	0.00005	0.0034	0.015
+Moment of inertia (I/MR2)	0.07	0.3308	0.212
+Visual magnitude V(1,0)	-26.74	-3.86	-
---
+++ pg.js
@@ -0,0 +1,6 @@
+const x = 0;
+
+const cl = console.log;
+const HL = 'Hell!!!';
+
+cl(HL);
